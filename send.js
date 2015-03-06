289903f1d612ea08bb6541e99d6be9aa06b36d84
John Mahowald <John.Mahowald@gmail.com>
2015-03-06 15:18:48 -0800

    create a file for sending messages

--- send.js
+++ send.js
@@ -4,5 +4,26 @@ var aws = require( "aws-sdk" );
 var Q = require ( "q" );
 var chalk = require( "chalk" );
 
+console.log(config);
+
 var sqs = new aws.SQS({
+  region: config.aws.region,
+  accessKeyId: config.aws.accessID,
+  secretAccessKey: config.aws.secretKey
+
+  params: {
+    QueueUrl: config.aws.queueUrl
+  }
+})
+
+var sendMessage = Q.nbimd( sqa.sendMessage, sqs );
+
+sendMessage({
+  MessageBody: "This is my first ever SQS request!"
+})
+.then( function handleSendResolve( data) {
+  console.log( chalk.green( "Message sent:", data.MessageId ) );
+})
+.catch( function handleReject( error) {
+  console.log( chalk.red( "Unexpected Rerror:", error.message ) );
 })
